80eb63b00d9a6b3e9367fd29855ea0ffa3de1361
Mpino19 <mykmm19@gmail.com>
2024-07-18 18:13:50 +0900

    cardをコンポーネント化

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,9 @@
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import Link from "next/link";
-import Image from "next/image";
+import BBSCardList from "./components/BBSCardList";
 
 export default function Home() {
   return (
     <main>
-      <Card>
-        <CardHeader>
-          <CardTitle>Create project</CardTitle>
-          <CardDescription>Deploy your new project in one-click.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit similique placeat ipsum! Sunt quam inventore unde, officiis ullam porro repellat consequatur ducimus exercitationem voluptatum voluptatem asperiores corporis harum. Quis, cumque?
-        </CardContent>
-        <CardFooter className="flex justify-between">
-          <Link href={"/"}>Read More</Link>
-        </CardFooter>
-      </Card>
-      <Card>
-        <CardHeader>
-          <CardTitle>Create project</CardTitle>
-          <CardDescription>Deploy your new project in one-click.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit similique placeat ipsum! Sunt quam inventore unde, officiis ullam porro repellat consequatur ducimus exercitationem voluptatum voluptatem asperiores corporis harum. Quis, cumque?
-        </CardContent>
-        <CardFooter className="flex justify-between">
-          <Link href={"/"}>Read More</Link>
-        </CardFooter>
-      </Card>
+      <BBSCardList />
     </main>
   );
 }
